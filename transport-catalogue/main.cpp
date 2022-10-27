55c0401b5c32582c42babe541a999cc8b0686b07
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2022-10-27 17:11:33 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,12 @@
-// место для вашего кода
+#include "transport_catalogue.h"
+#include "input_reader.h"
+#include "stat_reader.h"
+
+int main()
+{
+    transport::Catalogue transport_catalogue;
+    transport::query::InputReader(transport_catalogue);
+    transport::print::OutputReader(transport_catalogue);
+
+    return 0;
+}

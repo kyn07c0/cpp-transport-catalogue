22578dbc57ca690d57d862bf4bac5e6ebbcb7bfd
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2022-10-27 17:14:48 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,1 +1,77 @@
-// место для вашего кода
+#pragma once
+
+#include "geo.h"
+#include <string>
+#include <deque>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#include <set>
+
+namespace transport {
+namespace detail {
+
+    struct Stop
+    {
+        Stop(std::string name, geo::Coordinates coordinates) : name_(std::move(name)), coordinates_(coordinates)
+        {
+        }
+
+        std::string name_;
+        geo::Coordinates coordinates_;
+    };
+
+    struct Route
+    {
+        Route(std::string name, std::deque<Stop*> stops) : name_(std::move(name)), stops_(std::move(stops))
+        {
+        }
+
+        std::string name_;
+        std::deque<Stop*> stops_;
+    };
+
+    struct StopInfo
+    {
+        std::string name_;
+        std::set<std::string_view> buses_;
+        bool is_exist = true;
+    };
+
+    struct RouteInfo
+    {
+        std::string_view name_;
+        size_t stops_count_ = 0;
+        size_t unique_stops_count_ = 0;
+        uint32_t distance_ = 0;
+        double curvature_ = 0.0;
+    };
+
+} // namespace detail
+
+    class Catalogue
+    {
+        public:
+            void AddStop(const std::string& name, double lat, double lng);
+            void AddRoute(const std::string& name, const std::vector<std::string>& stops);
+
+            detail::Stop* FindStop(const std::string& name) const;
+            detail::Route* FindRoute(const std::string& name) const;
+
+            void AddDistance(const std::string& stop1, const std::string& stop2, uint32_t distance);
+            uint32_t GetDistance(const std::string& stop1, const std::string& stop2) const;
+
+            detail::StopInfo GetStopInfo(const std::string& name) const;
+            detail::RouteInfo GetRouteInfo(const std::string& name) const;
+
+        private:
+            std::deque<detail::Stop> base_stops_;
+            std::deque<detail::Route> base_routes_;
+
+            std::unordered_map<std::string_view, detail::Stop*> stops_;
+            std::unordered_map<std::string_view, detail::Route*> routes_;
+            std::unordered_map<std::string_view, std::set<std::string_view>> stop_for_buses_;
+            std::unordered_map<std::string_view, std::unordered_map<std::string_view, uint32_t>> distance_between_stops_;
+    };
+
+} // namespace transport

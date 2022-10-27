ee7e69b736c5aad40594d4e288e407b51399e57b
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2022-10-27 17:10:54 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,23 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+#include <vector>
+#include <unordered_map>
+
+namespace transport::query {
+
+    struct Data
+    {
+        std::string name;
+        std::vector<std::string> values;
+    };
+
+    std::vector<std::string> Split(std::string& str, const std::string& delimiter);
+
+    std::unordered_map<std::string, Data> ParseStop(const std::string& query);
+    std::unordered_map<std::string, Data> ParseRoute(const std::string& query);
+
+    std::unordered_map<std::string, Data> Parse(const std::string& query);
+    void InputReader(transport::Catalogue& transport_catalogue);
+
+} // namespace transport::query

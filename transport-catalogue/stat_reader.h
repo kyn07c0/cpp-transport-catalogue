9750a853f4933184136c150fba698a02acac9fdc
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2022-10-27 17:13:06 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,14 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+namespace transport::print {
+
+    void OutputReader(transport::Catalogue& transport_catalogue);
+    void RouteInfo(transport::Catalogue& transport_catalogue, const std::string& query_info);
+    void StopInfo(transport::Catalogue& transport_catalogue, const std::string& query_info);
+
+} // namespace transport::print

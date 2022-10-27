3cd21308ae7b8af606f1dd0df372084585dbec66
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2022-10-27 19:47:15 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -4,6 +4,8 @@
 
 namespace geo
 {
+    
+    const double DR = 3.1415926535 / 180.;
 
     struct Coordinates
     {
@@ -28,9 +30,8 @@ namespace geo
             return 0;
         }
 
-        static const double dr = 3.1415926535 / 180.;
-        return acos(sin(from.lat * dr) * sin(to.lat * dr) +
-                    cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr)) * 6371000;
+        return acos(sin(from.lat * DR) * sin(to.lat * DR) +
+                    cos(from.lat * DR) * cos(to.lat * DR) * cos(abs(from.lng - to.lng) * DR)) * 6371000;
     }
 
 } // namespace geo

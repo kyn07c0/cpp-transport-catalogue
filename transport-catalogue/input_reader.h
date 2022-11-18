11b3eb0b82337f7e24760e27a1bb599fa952fbcc
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2022-11-18 23:58:36 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,26 +1,27 @@
 #pragma once
 
 #include "transport_catalogue.h"
+#include <iostream>
 #include <vector>
 #include <unordered_map>
 
 namespace transport::query {
 
-    struct Data
-    {
-        std::string name;
-        std::vector<std::string> values;
-    };
+struct Data
+{
+    std::string name;
+    std::vector<std::string> values;
+};
 
-    std::vector<std::string> Split(std::string& str, const std::string& delimiter);
+std::vector<std::string> Split(std::string& str, const std::string& delimiter);
 
-    std::unordered_map<std::string, Data> ParseStop(const std::string& query);
-    std::unordered_map<std::string, Data> ParseRoute(const std::string& query);
-    std::unordered_map<std::string, Data> Parse(const std::string& query);
-    
-    void AddStops(transport::Catalogue& catalogue, std::vector<transport::query::Data>& queries);
-    void AddRoutes(transport::Catalogue& catalogue, std::vector<transport::query::Data>& queries);
+std::unordered_map<std::string, Data> ParseStop(const std::string& query);
+std::unordered_map<std::string, Data> ParseRoute(const std::string& query);
+std::unordered_map<std::string, Data> Parse(const std::string& query);
 
-    transport::Catalogue ReadCatalogue(std::istream& in);
+void AddStops(transport::Catalogue& catalogue, std::vector<transport::query::Data>& queries);
+void AddRoutes(transport::Catalogue& catalogue, std::vector<transport::query::Data>& queries);
+
+transport::Catalogue ReadCatalogue(std::istream& in);
 
 } // namespace transport::query

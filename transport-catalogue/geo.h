751cf52fe316eb8021dafe2e9e6dc95f5f62f13c
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2022-11-18 23:57:04 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -4,8 +4,7 @@
 
 namespace geo
 {
-    
-    const double DR = 3.1415926535 / 180.;
+    const double DR = M_PI / 180.;
 
     struct Coordinates
     {
@@ -22,16 +21,6 @@ namespace geo
         }
     };
 
-    inline double ComputeDistance(Coordinates from, Coordinates to)
-    {
-        using namespace std;
-        if(from == to)
-        {
-            return 0;
-        }
-
-        return acos(sin(from.lat * DR) * sin(to.lat * DR) +
-                    cos(from.lat * DR) * cos(to.lat * DR) * cos(abs(from.lng - to.lng) * DR)) * 6371000;
-    }
+    double ComputeDistance(Coordinates from, Coordinates to);
 
 } // namespace geo

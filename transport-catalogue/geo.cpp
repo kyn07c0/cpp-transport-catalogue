002a1b3217085aab3a59ca32d26934787d1fade5
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2022-11-19 18:22:42 +0300

    Update geo.cpp

--- transport-catalogue/geo.cpp
+++ transport-catalogue/geo.cpp
@@ -1,6 +1,8 @@
 #include "geo.h"
 
 namespace geo {
+    
+inline const int RADIUS_EARTH = 6371000;
 
 double ComputeDistance(Coordinates from, Coordinates to)
 {
@@ -13,7 +15,7 @@ double ComputeDistance(Coordinates from, Coordinates to)
     return acos(sin(from.lat * DR) * sin(to.lat * DR)
                 + cos(from.lat * DR) * cos(to.lat * DR)
                 * cos(abs(from.lng - to.lng) * DR))
-                * 6371000;
+                * RADIUS_EARTH;
 }
 
 } // namespace geo

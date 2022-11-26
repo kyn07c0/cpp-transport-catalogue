31db1f3615d0dcc4bdddd2375c8b925a99786501
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2022-11-26 15:35:38 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -7,17 +7,22 @@
 #include <iostream>
 #include <memory>
 
-class JsonReader
-{
+namespace transport::request {
+
+class JsonReader {
 public:
-    JsonReader(transport::request::RequestHandler& request_handler);
+    JsonReader(RequestHandler& request_handler);
 
     svg::Color ReadColor(const json::Node& json_color);
+
     transport::renderer::RenderSettings GetRenderSettings(const json::Dict& json_render_settings);
 
     void Exec(std::istream& in, std::ostream& out);
-    void RequestsProcessing( const json::Array& json_stat_requests, std::ostream& out);
+
+    void RequestsProcessing(const json::Array& json_stat_requests, std::ostream& out);
 
 private:
-    transport::request::RequestHandler& request_handler_;
+    RequestHandler& request_handler_;
 };
+
+} // namespace transport::request

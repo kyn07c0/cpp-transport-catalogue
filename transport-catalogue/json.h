3fea24b160202c8f51a04cc308a5f8659d434a90
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2022-11-26 15:40:38 +0300

    Update json.h

--- transport-catalogue/json.h
+++ transport-catalogue/json.h
@@ -10,89 +10,88 @@ using namespace std::literals;
 
 namespace json {
 
-    class Node;
+class Node;
 
-    using Dict = std::map<std::string, Node>;
-    using Array = std::vector<Node>;
+using Dict = std::map<std::string, Node>;
+using Array = std::vector<Node>;
 
-    // Эта ошибка должна выбрасываться при ошибках парсинга JSON
-    class ParsingError : public std::runtime_error
-    {
-    public:
-        using runtime_error::runtime_error;
-    };
+// Эта ошибка должна выбрасываться при ошибках парсинга JSON
+class ParsingError : public std::runtime_error
+{
+public:
+    using runtime_error::runtime_error;
+};
 
-    class Node
-    {
-    public:
-        using Value = std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string>;
+class Node
+{
+public:
+    using Value = std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string>;
 
-        Node() = default;
+    Node() = default;
 
-        Node(int value);
-        Node(double value);
-        Node(bool value);
-        Node(std::string value);
-        Node(std::nullptr_t);
-        Node(Array array);
-        Node(Dict map);
+    Node(int value);
+    Node(double value);
+    Node(bool value);
+    Node(std::string value);
+    Node(std::nullptr_t);
+    Node(Array array);
+    Node(Dict map);
 
-        const Value& GetValue() const;
+    const Value& GetValue() const;
 
-        int AsInt() const;
-        double AsDouble() const;
-        bool AsBool() const;
-        const std::string& AsString() const;
-        const Array& AsArray() const;
-        const Dict& AsMap() const;
+    int AsInt() const;
+    double AsDouble() const;
+    bool AsBool() const;
+    const std::string& AsString() const;
+    const Array& AsArray() const;
+    const Dict& AsDict() const;
 
-        bool IsInt() const;
-        bool IsDouble() const;
-        bool IsPureDouble() const;
-        bool IsBool() const;
-        bool IsString() const;
-        bool IsNull() const;
-        bool IsArray() const;
-        bool IsDict() const;
-        bool IsMap() const;
+    bool IsInt() const;
+    bool IsDouble() const;
+    bool IsPureDouble() const;
+    bool IsBool() const;
+    bool IsString() const;
+    bool IsNull() const;
+    bool IsArray() const;
+    bool IsDict() const;
 
-        bool operator==(const Node& rhs) const;
-        bool operator!=(const Node& rhs) const;
+    bool operator==(const Node& rhs) const;
+    bool operator!=(const Node& rhs) const;
 
-    private:
-        Value value_;
-    };
+private:
+    Value value_;
+};
 
-    void PrintNode(const Node& node, std::ostream& out);
+void PrintNode(const Node& node, std::ostream& out);
 
-    template <typename Value>
-    void PrintValue(const Value& value, std::ostream& out)
-    {
-        out << value;
-    }
+template <typename Value>
+void PrintValue(const Value& value, std::ostream& out)
+{
+    out << value;
+}
 
-    void PrintValue(std::nullptr_t, std::ostream& out);
-    void PrintValue(bool boolean, std::ostream& out);
-    void PrintValue(const std::string& str, std::ostream& out);
-    void PrintValue(const Array& nodes, std::ostream& out);
-    void PrintValue(const Dict& nodes, std::ostream& out);
+void PrintValue(std::nullptr_t, std::ostream& out);
+void PrintValue(bool boolean, std::ostream& out);
+void PrintValue(const std::string& str, std::ostream& out);
+void PrintValue(const Array& nodes, std::ostream& out);
+void PrintValue(const Dict& nodes, std::ostream& out);
 
-    class Document
-    {
-    public:
-        explicit Document(Node root);
+class Document
+{
+public:
+    explicit Document(Node root);
 
-        const Node& GetRoot() const;
+    const Node& GetRoot() const;
 
-        bool operator==(const Document& rhs) const;
-        bool operator!=(const Document& rhs) const;
+    bool operator==(const Document& rhs) const;
+    bool operator!=(const Document& rhs) const;
 
-    private:
-        Node root_;
-    };
+private:
+    Node root_;
+};
 
-    Document Load(std::istream& input);
+Document Load(std::istream& input);
 
-    void Print(const Document& doc, std::ostream& output);
+void Print(const Document& doc, std::ostream& output);
 
 }  // namespace json

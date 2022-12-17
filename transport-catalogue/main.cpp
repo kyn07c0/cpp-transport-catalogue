64a1317df5aa7e253586f8e99bedf818635ba53d
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2022-12-17 18:54:35 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,4 +1,5 @@
 #include "transport_catalogue.h"
+#include "transport_router.h"
 #include "json_reader.h"
 #include "map_renderer.h"
 #include "request_handler.h"
@@ -6,9 +7,10 @@
 int main()
 {
     transport::Catalogue catalogue;
+    route::Router route;
     transport::renderer::MapRenderer map_renderer;
-    transport::request::RequestHandler request_handler(catalogue, map_renderer);
-    JsonReader json_reader(request_handler);
+    transport::request::RequestHandler request_handler(catalogue, route, map_renderer);
+    transport::request::JsonReader json_reader(request_handler);
     json_reader.Exec(std::cin, std::cout);
 
     return 0;

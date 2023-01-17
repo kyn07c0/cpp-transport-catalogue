3866171a753df5eeeebaa2a921f6ec619f73254c
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2023-01-17 10:16:42 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -4,6 +4,7 @@
 #include "transport_catalogue.h"
 #include "transport_router.h"
 #include "map_renderer.h"
+#include "serialization.h"
 #include <unordered_set>
 
 namespace transport::request {
@@ -11,37 +12,35 @@ namespace transport::request {
 class RequestHandler
 {
 public:
-    RequestHandler(transport::Catalogue& catalogue, route::Router& router, transport::renderer::MapRenderer& map_renderer);
+    RequestHandler(transport::Catalogue& transport_catalogue,
+                   route::TransportRouter& transport_router,
+                   transport::renderer::MapRenderer& map_renderer);
 
     void AddStop(const std::string& name, double lat, double lng);
     void AddBus(const std::string& name, const std::vector<std::string>& stops, bool is_roundtrip);
     void AddDistance(const std::string& stop1, const std::string& stop2, uint32_t distance);
-
-    const std::vector<const domain::Stop*> GetStops() const;
-    const std::vector<const domain::Bus*> GetBuses() const;
-    const std::vector<domain::Stop*> GetBusStops(const domain::Bus* bus);
-
+    std::vector<const domain::Stop*> GetStops() const;
+    std::vector<const domain::Bus*> GetBuses() const;
+    std::vector<domain::Stop*> GetBusStops(const domain::Bus* bus);
     double GetDistance(const std::string& stop1, const std::string& stop2) const;
-
     domain::Bus* FindBus(const std::string_view& name) const;
-
-    domain::StopInfo GetStopInfo(const std::string_view name) const;
-    domain::BusInfo GetBusInfo(const std::string_view name) const;
-    std::optional<route::RouteInfo> GetRouteInfo(const std::string_view from, const std::string_view to) const;
-
-    void SetRendererSettings(transport::renderer::RenderSettings&& renderer_settings);
+    domain::StopInfo GetStopInfo(std::string_view name) const;
+    domain::BusInfo GetBusInfo(std::string_view name) const;
 
     void SetRoutingSettings(route::Settings&& routing_settings);
-    void AddStopToRouter(const std::string_view name);
-    void AddWaitEdgeToRouter(const std::string_view stop_name);
-    void AddBusEdgeToRouter(const std::string_view stop_from, const std::string_view stop_to, const std::string_view bus_name, const int span_count, const int dist);
+    void AddStopToRouter(std::string_view name);
+    void AddWaitEdgeToRouter(std::string_view stop_name);
+    void AddBusEdgeToRouter(std::string_view stop_from, std::string_view stop_to, std::string_view bus_name, int span_count, int dist);
     void BuildRouter();
+    std::optional<route::RouteInfo> GetRouteInfo(std::string_view from, std::string_view to) const;
+
+    void SetRendererSettings(transport::renderer::Settings&& renderer_settings);
 
     svg::Document RenderMap() const;
 
 private:
-    transport::Catalogue& catalogue_;
-    route::Router& router_;
+    transport::Catalogue& transport_catalogue_;
+    route::TransportRouter& transport_router_;
     transport::renderer::MapRenderer& map_renderer_;
 };
 

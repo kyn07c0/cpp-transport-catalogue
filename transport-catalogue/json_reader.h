3eadbfd71e8eee8a8dbc24638ef20591b3e3138e
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2023-01-17 10:14:09 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -4,26 +4,35 @@
 #include "json.h"
 #include "request_handler.h"
 #include "map_renderer.h"
+#include "transport_router.h"
 #include <iostream>
 #include <memory>
 
 namespace transport::request {
 
-class JsonReader 
+class JsonReader
 {
 public:
-    JsonReader(RequestHandler& request_handler);
+    JsonReader(RequestHandler& request_handler,
+               transport::Catalogue& transport_catalogue,
+               transport::renderer::MapRenderer& map_renderer,
+               route::TransportRouter& transport_router);
 
     svg::Color ReadColor(const json::Node& json_color);
 
-    transport::renderer::RenderSettings GetRenderSettings(const json::Dict& json_render_settings);
+    transport::renderer::Settings GetRenderSettings(const json::Dict& json_render_settings);
+    route::Settings GetRoutingSettings(const json::Dict& json_routing_settings);
 
-    void Exec(std::istream& in, std::ostream& out);
+    void ExecMakeBase(std::istream& in);
+    void ExecProcessRequest(std::istream& in, std::ostream& out);
 
     void RequestsProcessing(const json::Array& json_stat_requests, std::ostream& out);
 
 private:
     RequestHandler& request_handler_;
+    transport::Catalogue& transport_catalogue_;
+    transport::renderer::MapRenderer& map_renderer_;
+    route::TransportRouter& transport_router_;
 };
 
 } // namespace transport::request
